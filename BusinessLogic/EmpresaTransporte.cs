d8d1f0ba7c08fef2cb7465dba8ed1335e9a9d371
Diego Balbi <diegobalbi1993@gmail.com>
2023-09-06 07:38:06 -0300

    Create EmpresaTransporte.cs

---
+++ BusinessLogic/EmpresaTransporte.cs
@@ -0,0 +1,31 @@
+﻿namespace BusinessLogic
+{
+    public class EmpresaTransporte : ICalculadorCotizacion
+    {
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public ICollection<Vehiculo> Vehiculos { get; set; }
+
+        private EmpresaTransporte() {}
+
+        public EmpresaTransporte(string nombre, string ubicacion) 
+        {
+            Nombre = nombre;
+            Ubicacion = ubicacion;
+            Vehiculos = new List<Vehiculo>();    
+        }
+
+        public double CalculadorCotizacion(double peso, double distancia)
+        {
+            double cotizacion = 0;
+            foreach (var vehiculo in Vehiculos)
+            {
+                cotizacion += vehiculo.CalculadorCotizacion(peso, cotizacion);
+            }
+
+            return cotizacion;
+
+            //return Vehiculos.Sum(v => v.CalculadorCotizacion(peso, distancia));
+        }
+    }
+}
